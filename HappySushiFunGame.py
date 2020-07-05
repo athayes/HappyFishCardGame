b5c02d8e6ac71264a2da37eed095d9d149c3f755
Rebecca Frailich <rebecca.frailich@gmail.com>
2020-07-05 18:42:26 -0400

    python backend

--- HappySushiFunGame.py
+++ HappySushiFunGame.py
@@ -1,39 +1,11 @@
 import random
 
-class Game:
-    SPECIALS = ["Miso", "Wasabi", "Menu", "Takeout", "Special", "Soy", "Spoon", "Chopsticks", "Tea", "Edamame"]
-    ROLLS = ["Maki", "Temaki", "Uramaki"]
-    APPETIZERS = ["Tofu", "Dumpling", "Tempura", "Sashimi", "Eel", "Onigiri"]
-    DESSERT = ["Pudding", "Ice Cream", "Fruit"]
-
-    INTERACTION_CARDS = ["Maki", "Temaki", "Miso", "Pudding"]
-    CARDS_TO_DEAL = {2:10, 3:10, 4:9, 5:9, 6:8, 7:8, 8:7}
-   
-    def __init__(self, deck, num_players, tableau_interaction, num_players)
-       self.round = 0
-       self.deck = deck
-       self.tableaus = [Tableau() for i in range(num_players)]
-       self.hands = [Hand([], self.tableaus[i]) for i in range(num_players)]
-       self.scores = [0 for i in range(num_players)]
-       self.num_players = num_players
-       self.board_interaction = tableau_interaction # if using maki, temaki, miso, or pudding
-
-    def start_round(self):
-        self.round += 1
-        self.deck.shuffle()
-        # How to keep pudding?
-        
-        for hand in self.hands:
-            self.deck.deal(hand, CARDS_TO_DEAL.get(self.num_players))
-
-    def score_round(self):
-        
-        
-
-
 class Deck:
-    def __init__(self, cards):
-        self.cards = cards
+    def __init__(self, card_types, num_to_deal):
+    
+        self.cards = [];
+        for card_type in card_types:
+            self.cards += [card_type() for i in range(num_to_deal)]
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -50,7 +22,7 @@ class Hand:
 
     def play(self, card):
         self.cards.remove(card)
-        self.tableu.append(card)
+        self.tableau.append(card)
 
 class Tableau:
     def __init__(self, cards):
@@ -58,138 +30,195 @@ class Tableau:
 
 
 class Card:
-    def __init__(self, face_value):
-        self.face_value = face_value
-        
+    face_value = 0
+    sort_value = 0
+    dessert = False
+
     def play(self):
         pass
-    
-    def score(self):
-        return self.face_value
-        
+
+    def __lt__(self, other):
+        return self.sort_value < other.sort_value
+
+    def __gt__(self, other):
+        return self.sort_value > other.sort_value
+
     def __str__(self):
         return self.__class__.__name__
+    
+    @classmethod
+    def score(clazz, tableaus, scores):
+        # Tableaus must be sorted
+        
+        j = 0
+        
+        for i in range(len(tableaus)):
+            while isinstace(tableau[j], clazz):
+                scores[i] += card.face_value
+                j += 1
+            tableau = tableau[j:-1]
+
 
 
 # Face value score cards
 class Egg(Card):
-    def __init__(self):
-        Super(self, 1)
+    face_value = 1
+    sort_value = 1
 
 class Salmon(Card):
-    def __init__(self):
-        Super(self, 2)
+    face_value = 2
+    sort_value = 2
 
 class Squid(Card):
-    def __init__(self):
-        Super(self, 3)
+    face_value = 3
+    sort_value = 3
         
 class Miso(Card):
-    def __init__(self):
-        Super(self, 3)
+    face_value = 3
+    sort_value = 4
 
 # No score cards       
 class Menu(Card):
-    def __init__():
-        Super(self, 0)
+    sort_value = 5
 
 class Takeout(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 6
 
 class Special(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 7
 
 class Soy(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 8
 
 class Spoon(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 9
 
 class Chopsticks(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 10
 
 class Tea(Card):
-    def __init__(self):
-        Super(self, 1)
+    sort_value = -1
 
 # Combo scoring cards
 class Wasabi(Card):
     def __init__(self):
-        Super(self, 0)
+        # score this first so linked nigiri is def not deleted
+        super().__init__()
         self.nigiri = none
         
-    def score(self);
-        if self.nigiri == none:
-            Super.score()
-        else:
-            return self.nigiri.face_value * 2
+    @classmethod
+    def score(clazz, tableaus, scores):
+        # Tableaus must be sorted
+        
+        j = 0
+        
+        for i in range(len(tableaus)):
+            while isinstace(tableau[j], Wasabi):
+                # The third will be added in the nigiri scoring method
+                scores[i] += card.nigiri.face_value * 2
+                j += 1
+            tableau = tableau[j:-1]
 
 class Tofu(Card):
-    def __init__(self):
-        Super(self, 2)
+    face_value = 2
+    sort_value = 11
 
 class Dumpling(Card):
-    def __init__(self):
-        Super(self, 1)
+    sort_value = 12
 
 class Tempura(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 13
 
 class Sashimi(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 14
         
     def score(self):
+        pass
 
 class Eel(Card):
-    def __init__(self):
-        Super(self, -3)
+    face_value = -3
+    sort_value = 15
 
 class Ice_Cream(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 100
+    dessert = True
         
 class Fruit(Card):
+    sort_value = 100
+    dessert = True
+
     def __init__(self, type):
-        Super(self, 0)
+        super().__init__()
         self.type = type
 
 # Other player-dependent cards
 class Maki(Card):
+    sort_value = 16
+    reward = 3
+    punishment = -3
+
     def __init__(self, power):
-        Super(self, 0)
+        super().__init__()
         self.power = power
-        self.reward = 3
-        self.punishment = -3
 
 class Temaki(Card):
-    def __init__(self):
-        Super(self, 0)
-        self.reward = 4
-        self.punishment = -4
+    sort_value = 17
+    reward = 4
+    punishment = -4
         
 class Uramaki(Card):
+    sort_value = 20
+
     def __init__(self, power):
-        Super(self, 0)
+        super().__init__()
         self.power = power
 
 class Edamame(Card):
-    def __init__(self):
-        Super(self, 0)
+    sort_value = 18
+
 
 class Onigiri(Card):
+    face_value = 1
+    sort_value = 19
+
     def __init__(self, type):
-        Super(self, 1)
+        super().__init__()
         self.type = type
 
 class Pudding(Card):
-    def __init__(self):
-        Super(self, 0)
-        self.reward = 6
-        self.punishment = -6
+    sort_value = 100
+    dessert = True
+    reward = 6
+    punihsment = -6
+
+class Game:
+    SPECIALS = [Miso, Wasabi, Menu, Takeout, Special, Soy, Spoon, Chopsticks, Tea, Edamame]
+    ROLLS = [Maki, Temaki, Uramaki]
+    APPETIZERS = [Tofu, Dumpling, Tempura, Sashimi, Eel, Onigiri]
+    DESSERT = [Pudding, Ice_Cream, Fruit]
+
+    INTERACTION_CARDS = [Maki, Temaki, Miso, Pudding]
+    CARDS_TO_DEAL = {2:10, 3:10, 4:9, 5:9, 6:8, 7:8, 8:7}
+   
+    def __init__(self, num_players, tableau_interaction, cards_in_use):
+        self.cards_in_use = cards_in_use
+        self.rnd = 0
+        self.deck = Deck(cards_in_use, CARDS_TO_DEAL[num_players])
+        self.tableaus = [Tableau() for i in range(num_players)]
+        self.hands = [Hand([], self.tableaus[i]) for i in range(num_players)]
+        self.scores = [0 for i in range(num_players)]
+        self.num_players = num_players
+
+    def start_round(self):
+        self.round += 1
+        self.deck.shuffle()
+        # How to keep pudding?
+        
+        for hand in self.hands:
+            self.deck.deal(hand, CARDS_TO_DEAL.get(self.num_players))
+
+    def score_round(self):
+        for tableau in tableaus:
+            tableau.sort()
+        for card_type in cards_in_use:
+            card_type.score(card_type, tableaus, scores)

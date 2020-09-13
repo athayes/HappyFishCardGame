405febe24ad506e27fbc7f1271f57ee5315a4354
Rebecca Frailich <rebecca.frailich@icloud.com>
2020-09-13 22:38:17 +0000

    Naming and cleanup

---
+++ backend/routes.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+import HappySushiFunGame as fish
+app = Flask(__name__)
+
+game_id = None
+
+@app.route('/startGame', methods=['POST'])
+def start_game():
+    fish.Feck_It.start_game(game_id)
+
+# Recreate game 1
+@app.route('/creategame', methods=['POST'])
+def create_game():
+    player = request.json['player']
+    game_id = fish.Feck_It.add_game()
+    fish.Feck_It.add_player(player, game_id)
+    return game_id
+
+@app.route('/joingame', methods=['POST'])
+def join():
+    if game_id != null:
+        player = request.json['player']
+        fish.Feck_It.add_player(player, game_id)
+    else:
+        return 'You have to create a game first!'

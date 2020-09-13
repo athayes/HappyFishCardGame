c296d0fad15d7658620a826b975422b2953a8b17
Anthony Hayes <athayes.ca@gmail.com>
2020-09-13 19:26:01 -0400

    Fix cors

--- backend/routes.py
+++ backend/routes.py
@@ -3,24 +3,23 @@ from flask import request
 import HappySushiFunGame as fish
 from flask_cors import CORS
 
+game_id = None
 app = Flask(__name__)
 CORS(app)
 
-game_id = None
-
-@app.route('/startGame', methods=['POST'])
+@app.route('/StartGame', methods=['POST'])
 def start_game():
     fish.Feck_It.start_game(game_id)
 
 # Recreate game 1
-@app.route('/creategame', methods=['POST'])
+@app.route('/CreateGame', methods=['POST'])
 def create_game():
-    player = request.json['player']
+    player = request.json['hostName']
     game_id = fish.Feck_It.add_game()
     fish.Feck_It.add_player(player, game_id)
     return game_id
 
-@app.route('/joingame', methods=['POST'])
+@app.route('/JoinGame', methods=['POST'])
 def join():
     if game_id != null:
         player = request.json['player']

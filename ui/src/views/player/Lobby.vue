3ad88886123d27b7b9f4c402b3dd79e8917bf470
Anthony Hayes <athayes.ca@gmail.com>
2020-10-04 20:18:10 -0400

    Host lobby done

--- ui/src/views/player/Lobby.vue
+++ ui/src/views/player/Lobby.vue
@@ -4,22 +4,43 @@
     <div class="userTable paper container container-xs">
       <table class="lobbyTable">
         <tbody>
-          <tr>
-            <td>Bob</td>
-          </tr>
-          <tr>
-            <td>Eric</td>
-          </tr>
-          <tr>
-            <td>Daniel</td>
+          <tr v-for="player in players" :key="player">
+            <td>{{ player }}</td>
           </tr>
         </tbody>
       </table>
     </div>
     <h3>Waiting for x to start the game...</h3>
-
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      players: [],
+      interval: null
+    };
+  },
+  async created() {
+    let self = this;
+    let response = await axios.post("http://127.0.0.1:5000/GetPlayersInLobby");
+    self.players = response.data.players;
+
+    self.interval = setInterval(async () => {
+      let response = await axios.post(
+        "http://127.0.0.1:5000/GetPlayersInLobby"
+      );
+      self.players = response.data.players;
+      console.log(response.data.players);
+    }, 5 * 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  }
+};
+</script>
 
 <style>
 .Lobby {

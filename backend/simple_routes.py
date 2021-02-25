2fffeab6121691dbf6012838abd924c83822e1c7
Anthony <athayes.ca@gmail.com>
2021-02-24 23:28:29 -0500

    Split out lobby into its own file

--- backend/simple_routes.py
+++ backend/simple_routes.py
@@ -1,16 +1,10 @@
 from flask import Flask, json
-
 from flask import request
 from flask_cors import CORS
-
-
+from src.lobby import Lobby
 
 app = Flask(__name__)
 CORS(app)
-
-
-
-
 
 @app.route('/StartGame', methods=['POST'])
 def start_game():

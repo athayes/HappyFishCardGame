aed2c346f048dd91337a6650032c3ab8d34965e4
Anthony <athayes.ca@gmail.com>
2021-03-21 20:18:42 -0400

    Silly words working! :)

--- ui/src/models/SillyWords.js
+++ ui/src/models/SillyWords.js
@@ -2,3 +2,7 @@ export const sillyWords = {
   nouns: ["Sushi", "Restaurant", "Chef", "Dinner"],
   adjectives: ["Happy", "Exciting", "Delicious", "Good Taste"]
 };
+
+export function getRandomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}

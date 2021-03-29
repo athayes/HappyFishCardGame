933adacb28b8b2cf8fbb5a8ac5debc7127462660
Anthony <athayes.ca@gmail.com>
2021-03-28 23:15:12 -0400

    Fun colors

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -35,6 +35,23 @@
   background-color: #ddf;
 }
 
+.blue-button {
+  background-color: #deefff;
+  border-color: #6ab;
+  /*background-color: #cff;*/
+  /*border-color: #0071de;*/
+}
+
+.pink-button {
+  background-color: #fdd;
+  border-color: #f99;
+}
+
+.pinkish-button {
+  background-color: #edc;
+  border-color: #f99;
+}
+
 /* Look into using default for focus*/
 /*input:focus, select:focus, textarea:focus {*/
 /*  border: 2px solid #0071de;*/

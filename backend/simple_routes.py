2738e5132a281e42013ff9fba92630d9cec6a100
Anthony <athayes.ca@gmail.com>
2021-04-18 19:51:04 -0400

    Try deploy heroku

--- backend/simple_routes.py
+++ backend/simple_routes.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*", async_handlers=True)
 
-# if __name__ == '__main__':
-#     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
 
 @app.route('/', methods=['GET'])
 def root():

fe29204d2ee9ae5be5c25869f2c1481a098dd7f7
Anthony <athayes.ca@gmail.com>
2021-04-25 21:41:27 -0400

    Whoops

--- backend/simple_routes.py
+++ backend/simple_routes.py
@@ -78,7 +78,7 @@ def pick_card():
     if Lobby.game.game_state == "COMPLETED":
         Lobby.last_finished_game = deepcopy(Lobby.game)
         Lobby.game = None
-        Lobby.players = None
+        Lobby.players = []
         push_game_end()
     else:
         push_game_data()

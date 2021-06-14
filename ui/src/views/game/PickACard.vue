1e7070bb602b227a669da1997489bd46c71d94ec
Anthony <athayes.ca@gmail.com>
2021-06-13 21:40:07 -0400

    Force updates

--- ui/src/views/game/PickACard.vue
+++ ui/src/views/game/PickACard.vue
@@ -6,7 +6,7 @@
       </div>
       <h3>Pick a card</h3>
       <div class="hand">
-        <div class="card" v-for="(card, index) in currentPlayer.hand" :key="card.index" @click.stop="chooseCard(card, index)">
+        <div class="card" v-for="(card, index) in currentPlayer.hand" @click.stop="chooseCard(card, index)">
           <img v-bind:src="card.image" />
           <div>
             <p class="name">{{ card.name }}</p>
@@ -182,6 +182,7 @@ export default {
       this.playerIndex = index;
       this.tableauIndex = index;
       this.players = players;
+      this.$forceUpdate();
     },
 
     refreshDataGameEnd: async function() {

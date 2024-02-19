4df88eec4954553d6b57d44f622ab43c4c8d219e
Anthony Hayes <110310587@umail.ucc.ie>
2024-02-19 01:45:29 +0000

    wip

---
+++ rewrite/backend/src/main.test.ts
@@ -0,0 +1,17 @@
+jest.mock('./app', () => {
+    return {
+      app: {
+        listen: jest.fn(),
+        get: jest.fn(),
+      },
+    };
+  });
+  
+  describe('main', () => {
+    it('calls serverApp.listen with correct arguments', async () => {
+      const { app } = await import('./app');
+      await import('./main');
+  
+      expect(app.listen).toHaveBeenCalledWith(3000, expect.any(Function));
+    });
+  });
